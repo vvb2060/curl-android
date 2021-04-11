46e8624e57a441020d82cc019945d097804ebdf0
vvb2060 <vvb2060@gmail.com>
2021-04-12 00:29:28 +0800

    Publish to maven local

--- curl/src/main/native/Android.mk
+++ curl/src/main/native/Android.mk
@@ -7,7 +7,10 @@ LOCAL_SRC_FILES         := $(addprefix curl/lib/,$(CSOURCES))
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/curl/include $(LOCAL_PATH)/curl/lib $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/curl/include
 LOCAL_CFLAGS            := -DHAVE_CONFIG_H -DBUILDING_LIBCURL
+LOCAL_STATIC_LIBRARIES  := ssl_static
 # https://gist.github.com/vvb2060/56d5b8fda2553f36938b2b72b1390114
 LOCAL_STATIC_OBJCOPY    := true
 LOCAL_OBJCOPY_MODE      := --strip-unneeded -R .comment
 include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,prefab/boringssl)
