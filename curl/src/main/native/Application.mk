517aae66b7712ae82f7224f562c68b78e26105fe
vvb2060 <vvb2060@gmail.com>
2023-03-18 01:00:55 +0800

    Enable LTO

--- curl/src/main/native/Application.mk
+++ curl/src/main/native/Application.mk
@@ -1,4 +1,4 @@
-APP_CFLAGS     := -Wall -Wextra -Werror
+APP_CFLAGS     := -Wall -Wextra -Wmost -Werror
 APP_CFLAGS     += -Wno-builtin-macro-redefined -D__FILE__=__FILE_NAME__
-APP_CONLYFLAGS := -std=c18
+APP_CONLYFLAGS := -std=c2x
 APP_STL        := none
--- example/src/main/native/Application.mk
+++ example/src/main/native/Application.mk
@@ -1,5 +1,10 @@
-APP_CFLAGS     := -Wall -Wextra -Werror
-APP_CFLAGS     += -fvisibility=hidden -fvisibility-inlines-hidden
-APP_CONLYFLAGS := -std=c18
-APP_LDFLAGS    := -Wl,-exclude-libs,ALL
+APP_CFLAGS     := -Wall -Wextra -Wmost
+APP_CFLAGS     += -Wno-builtin-macro-redefined -D__FILE__=__FILE_NAME__
+APP_CONLYFLAGS := -std=c2x
 APP_STL        := none
+
+ifneq ($(NDK_DEBUG),1)
+APP_CFLAGS     += -flto -Werror
+APP_CFLAGS     += -fvisibility=hidden -fvisibility-inlines-hidden
+APP_LDFLAGS    += -flto -Wl,--exclude-libs,ALL
+endif
